6c12aabefab8a163c21833c72530cfb489751ec0
Fredrick Ochieng <flaughters@gmail.com>
2022-10-06 06:45:00 +0300

    Add project files.

---
+++ CSharpAdvanced/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i%2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+       /* 
+          var name = "Fredrick Ochieng0";
+        foreach(var character in name)
+        {
+            Console.WriteLine(character);
+        }
+       */
+
+        var random = new Random();
+        var buffer = new char[10];
+
+        for (var i = 0; i < 10; i++)
+            //Console.WriteLine(random.Next());
+            buffer[i] = (char)('a' + random.Next(0, 26));
+
+        var password = new string(buffer);
+            Console.WriteLine(password);
+        
+    }
+}
